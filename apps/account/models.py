1f34e4c07080e49d1a4b0c6fa14145edd5d3102e
ming.chen <ming.chen26@pactera.com>
2022-01-07 10:45:07 +0800

    new file:   apps/account/__init__.py 	new file:   apps/account/admin.py 	new file:   apps/account/apps.py 	new file:   apps/account/migrations/__init__.py 	new file:   apps/account/models.py 	new file:   apps/account/static/bootstrap-3.3.7-dist/css/bootstrap-theme.css 	new file:   apps/account/static/bootstrap-3.3.7-dist/css/bootstrap-theme.css.map 	new file:   apps/account/static/bootstrap-3.3.7-dist/css/bootstrap-theme.min.css 	new file:   apps/account/static/bootstrap-3.3.7-dist/css/bootstrap-theme.min.css.map 	new file:   apps/account/static/bootstrap-3.3.7-dist/css/bootstrap.css 	new file:   apps/account/static/bootstrap-3.3.7-dist/css/bootstrap.css.map 	new file:   apps/account/static/bootstrap-3.3.7-dist/css/bootstrap.min.css 	new file:   apps/account/static/bootstrap-3.3.7-dist/css/bootstrap.min.css.map 	new file:   apps/account/static/bootstrap-3.3.7-dist/fonts/glyphicons-halflings-regular.eot 	new file:   apps/account/static/bootstrap-3.3.7-dist/fonts/glyphicons-halflings-regular.svg 	new file:   apps/account/static/bootstrap-3.3.7-dist/fonts/glyphicons-halflings-regular.ttf 	new file:   apps/account/static/bootstrap-3.3.7-dist/fonts/glyphicons-halflings-regular.woff 	new file:   apps/account/static/bootstrap-3.3.7-dist/fonts/glyphicons-halflings-regular.woff2 	new file:   apps/account/static/bootstrap-3.3.7-dist/js/bootstrap.js 	new file:   apps/account/static/bootstrap-3.3.7-dist/js/bootstrap.min.js 	new file:   apps/account/static/bootstrap-3.3.7-dist/js/npm.js 	new file:   apps/account/static/css/login.css 	new file:   apps/account/static/js/jquery-3.2.1.js 	new file:   apps/account/templates/base.html 	new file:   apps/account/templates/confirm.html 	new file:   apps/account/templates/index.html 	new file:   apps/account/templates/login.html 	new file:   apps/account/templates/register.html 	new file:   apps/account/views.py 	new file:   recite_words/settings_dev.py

---
+++ apps/account/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+# Create your models here.
+class User(models.Model):
+    gender_choice = (
+        (0, "男"),
+        (1, "女")
+    )
+    level_choice = (
+        (0, "普通用户"),
+        (1, "高级用户")
+    )
+    username = models.CharField(verbose_name='用户名', max_length=32, unique=True)
+    password = models.CharField(verbose_name='密码', max_length=128)
+    email = models.EmailField(verbose_name='邮箱', blank=True, null=True)
+    gender = models.SmallIntegerField(verbose_name='性别', choices=gender_choice, blank=True, null=True)
+    create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
+    level = models.SmallIntegerField(verbose_name='用户等级', default=0)
+    avatar = models.CharField()
+
+    # has_confirmed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
