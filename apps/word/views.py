e2a2848d9e693e85f6cc4d53d9981e2ad4ddff7b
ming.chen <ming.chen26@pactera.com>
2022-01-10 18:02:38 +0800

    modified:   apps/word/templates/words.html 	modified:   apps/word/views.py 	modified:   utils/pagination.py

--- apps/word/views.py
+++ apps/word/views.py
@@ -1,6 +1,139 @@
 from django.shortcuts import render, HttpResponse
+from django.utils.safestring import mark_safe
 from apps.word import models
 from utils.pagination import Pagination
+
+
+class CheckFilter(object):
+    def __init__(self, name, data_list, request):
+        self.name = name
+        self.data_list = data_list
+        self.request = request
+
+    def __iter__(self):
+        for item in self.data_list:
+            key = str(item[0])
+            text = item[1]
+            ck = ""
+            # 如果当前用户请求的URL中status和当前循环key相等
+            value_list = self.request.GET.getlist(self.name)
+            if key in value_list:
+                ck = 'checked'
+                value_list.remove(key)
+            else:
+                value_list.append(key)
+
+            # 为自己生成URL
+            # 在当前URL的基础上去增加一项
+            # status=1&age=19
+            query_dict = self.request.GET.copy()
+            query_dict._mutable = True
+            query_dict.setlist(self.name, value_list)
+            if 'page' in query_dict:
+                query_dict.pop('page')
+
+            param_url = query_dict.urlencode()
+            if param_url:
+                url = "{}?{}".format(self.request.path_info, param_url)  # status=1&status=2&status=3&xx=1
+            else:
+                url = self.request.path_info
+
+            tpl = '<a class="cell" href="{url}"><input type="checkbox" {ck} /><label>{text}</label></a>'
+            html = tpl.format(url=url, ck=ck, text=text)
+            yield mark_safe(html)
+
+
+class SelectFilter(object):
+    def __init__(self, name, data_list, request):
+        self.name = name
+        self.data_list = data_list
+        self.request = request
+
+    def __iter__(self):
+        yield mark_safe("<select class='select2' multiple='multiple' style='width:100%;' >")
+        for item in self.data_list:
+            key = str(item[0])
+            text = item[1]
+
+            selected = ""
+            value_list = self.request.GET.getlist(self.name)
+            if key in value_list:
+                selected = 'selected'
+                value_list.remove(key)
+            else:
+                value_list.append(key)
+
+            query_dict = self.request.GET.copy()
+            query_dict._mutable = True
+            query_dict.setlist(self.name, value_list)
+            if 'page' in query_dict:
+                query_dict.pop('page')
+
+            param_url = query_dict.urlencode()
+            if param_url:
+                url = "{}?{}".format(self.request.path_info, param_url)  # status=1&status=2&status=3&xx=1
+            else:
+                url = self.request.path_info
+
+            html = "<option value='{url}' {selected} >{text}</option>".format(url=url, selected=selected, text=text)
+            yield mark_safe(html)
+        yield mark_safe("</select>")
+
+
+class SearchGroupRow(object):
+    def __init__(self, title, queryset_or_tuple, option, query_dict):
+        """
+        :param title: 组合搜索的列名称
+        :param queryset_or_tuple: 组合搜索关联获取到的数据
+        :param option: 配置
+        :param query_dict: request.GET
+        """
+        self.title = title
+        self.queryset_or_tuple = queryset_or_tuple
+        self.option = option
+        self.query_dict = query_dict
+
+    def __iter__(self):
+        yield '<div class="whole">'
+        yield self.title
+        yield '</div>'
+        yield '<div class="others">'
+        total_query_dict = self.query_dict.copy()
+        total_query_dict._mutable = True
+
+        origin_value_list = self.query_dict.getlist(self.option.field)
+        if not origin_value_list:
+            yield "<a class='active' href='?%s'>全部</a>" % total_query_dict.urlencode()
+        else:
+            total_query_dict.pop(self.option.field)
+            yield "<a href='?%s'>全部</a>" % total_query_dict.urlencode()
+
+        for item in self.queryset_or_tuple:
+            text = self.option.get_text(item)
+            value = str(self.option.get_value(item))
+            query_dict = self.query_dict.copy()
+            query_dict._mutable = True
+
+            if not self.option.is_multi:
+                query_dict[self.option.field] = value
+                if value in origin_value_list:
+                    query_dict.pop(self.option.field)
+                    yield "<a class='active' href='?%s'>%s</a>" % (query_dict.urlencode(), text)
+                else:
+                    yield "<a href='?%s'>%s</a>" % (query_dict.urlencode(), text)
+            else:
+                # {'gender':['1','2']}
+                multi_value_list = query_dict.getlist(self.option.field)
+                if value in multi_value_list:
+                    multi_value_list.remove(value)
+                    query_dict.setlist(self.option.field, multi_value_list)
+                    yield "<a class='active' href='?%s'>%s</a>" % (query_dict.urlencode(), text)
+                else:
+                    multi_value_list.append(value)
+                    query_dict.setlist(self.option.field, multi_value_list)
+                    yield "<a href='?%s'>%s</a>" % (query_dict.urlencode(), text)
+
+        yield '</div>'
 
 
 # Create your views here.
@@ -15,6 +148,7 @@ def word_list(request):
     words_obj_list = queryset[page_object.start:page_object.end]
     context = {
         'word_object_list': words_obj_list,
-        'page_html': page_object.page_html()
+        'page_html': page_object.page_html(),
+
     }
     return render(request, 'words.html', context)
